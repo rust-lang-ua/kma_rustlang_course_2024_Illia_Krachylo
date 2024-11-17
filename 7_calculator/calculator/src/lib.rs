132f427b85799ad11aa79d9aca742e9130f369a2
Illia Krachylo <kraciloi@gmail.com>
2024-11-17 22:31:21 +0200

    final variant of lab 7 calculator

--- 7_calculator/calculator/src/lib.rs
+++ 7_calculator/calculator/src/lib.rs
@@ -8,6 +8,7 @@ pub struct CalculatorParser;
 #[derive(Debug)]
 pub enum Expr {
     Integer(i32),
+    UnaryMinus(Box<Expr>),
     BinOp {
         lhs: Box<Expr>,
         op: Op,
@@ -21,6 +22,7 @@ pub enum Op {
     Subtract,
     Multiply,
     Divide,
+    Modulo,
 }
 
 lazy_static::lazy_static! {
@@ -30,7 +32,8 @@ lazy_static::lazy_static! {
 
         PrattParser::new()
             .op(Op::infix(add, Left) | Op::infix(subtract, Left))
-            .op(Op::infix(multiply, Left) | Op::infix(divide, Left))
+            .op(Op::infix(multiply, Left) | Op::infix(divide, Left) | Op::infix(modulo, Left))
+            .op(Op::prefix(unary_minus))
     };
 }
 
@@ -38,7 +41,8 @@ pub fn parse_expr(pairs: Pairs<Rule>) -> Expr {
     PRATT_PARSER
         .map_primary(|primary| match primary.as_rule() {
             Rule::integer => Expr::Integer(primary.as_str().parse::<i32>().unwrap()),
-            rule => unreachable!("Expr::parse expected atom, found {:?}", rule)
+            Rule::expr => parse_expr(primary.into_inner()),
+            rule => unreachable!("Expr::parse expected atom, found {:?}", rule),
         })
         .map_infix(|lhs, op, rhs| {
             let op = match op.as_rule() {
@@ -46,6 +50,7 @@ pub fn parse_expr(pairs: Pairs<Rule>) -> Expr {
                 Rule::subtract => Op::Subtract,
                 Rule::multiply => Op::Multiply,
                 Rule::divide => Op::Divide,
+                Rule::modulo => Op::Modulo,
                 rule => unreachable!("Expr::parse expected infix operation, found {:?}", rule),
             };
             Expr::BinOp {
@@ -54,5 +59,29 @@ pub fn parse_expr(pairs: Pairs<Rule>) -> Expr {
                 rhs: Box::new(rhs),
             }
         })
+        .map_prefix(|op, rhs| match op.as_rule() {
+            Rule::unary_minus => Expr::UnaryMinus(Box::new(rhs)),
+            _ => unreachable!(),
+        })
         .parse(pairs)
+}
+
+impl Expr {
+    pub fn evaluate(&self) -> i32 {
+        match self {
+            Expr::Integer(value) => *value,
+            Expr::UnaryMinus(expr) => -expr.evaluate(),
+            Expr::BinOp { lhs, op, rhs } => {
+                let left = lhs.evaluate();
+                let right = rhs.evaluate();
+                match op {
+                    Op::Add => left + right,
+                    Op::Subtract => left - right,
+                    Op::Multiply => left * right,
+                    Op::Divide => left / right,
+                    Op::Modulo => left % right,
+                }
+            }
+        }
+    }
 }
--- 7_calculator/calculator/src/main.rs
+++ 7_calculator/calculator/src/main.rs
@@ -1,13 +1,14 @@
-use std::io::{self, BufRead};
 use calculator::*;
 use pest::Parser;
+use std::io::{self, BufRead};
 
 fn main() -> io::Result<()> {
     for line in io::stdin().lock().lines() {
         match CalculatorParser::parse(Rule::equation, &line?) {
             Ok(mut pairs) => {
-                println!("Parsed: {:#?}", parse_expr(pairs.next().unwrap().into_inner()));
-            },
+                let expr = parse_expr(pairs.next().unwrap().into_inner());
+                println!("Result: {}", expr.evaluate());
+            }
             Err(e) => {
                 println!("Parse failed: {:?}", e);
             }
